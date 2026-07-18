c1145d19aa9729f9872af4ff457fdc3ae4ed81d0
hogiakham <hogiakham1705@gmail.com>
2026-07-18 18:54:26 +0700

    Update API link for Frontend deployment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,6 @@
-export const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000/api";
+// export const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000/api";
+// export const API_HOST = API_BASE.replace(/\/api\/?$/, "");
+
+
+export const API_BASE = process.env.REACT_APP_API_BASE || "https://edutestonline.onrender.com/api";
 export const API_HOST = API_BASE.replace(/\/api\/?$/, "");
